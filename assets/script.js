2cfd71714678ed2f913bb2ee322e5533da5323e9
Jessica Sanchez <jessamesan@hotmail.com>
2023-04-21 23:56:40 -0600

    Previous push had incorrect files. This new has the correct ones and fixes duplicate cities in search storage, corrects current and fore cast positions

--- assets/script.js
+++ assets/script.js
@@ -1,94 +1,107 @@
-// var apiKey = 'c8a50d3cad8319246b6b9747e912dec7';
+const apiKey = 'b9e5d754e2eb97d65075459b2875fbf5';
+const forecastDiv = document.getElementById('forecast');
+var searchHistoryEl = document.getElementById("search-history")
 
 
+function clearForecast() {
+  forecastDiv.innerHTML = '';
+}
 
-/*function getWeather(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+function handleFormSubmit(event) {
+  event.preventDefault();
+  let city = document.getElementById('city').value;
+  city = city.toLowerCase();
+  city = city.charAt(0).toUpperCase() + city.slice(1);
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  clearForecast();
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const temperature = data.main.temp;
-      const humidity = data.main.humidity;
-      const windSpeed = data.wind.speed;
-      const uvIndex = data.visibility;
-      const clouds = data.clouds.all;
-      const resultDiv = document.getElementById('result');
-      resultDiv.innerHTML = `
-        <h3>${city}</h3>
-        <p>Temperature: ${temperature} °C</p>
-        <p>Humidity: ${humidity}%</p>
-        <p>Wind Speed: ${windSpeed} m/s</p>
-        <p>UV Index: ${uvIndex}</p>
-        <p>Clouds: ${clouds}%</p>
-      `;
-    })
-    .catch(error => {
-      console.error('Error fetching weather data:', error);
-      const resultDiv = document.getElementById('result');
-      resultDiv.innerHTML = '<p>Error fetching weather data</p>';
-    });
-}
-
-const form = document.querySelector('form');
-form.addEventListener('click', event => {
-  event.preventDefault();
-  const cityInput = document.getElementById('city');
-  const city = cityInput.value;
-  getWeather(city);
-});
-*/
-const API_KEY = 'c8a50d3cad8319246b6b9747e912dec7';
-const form = document.querySelector('form');
-const currentWeather = document.getElementById('current-weather');
-const forecastTable = document.getElementById('forecast-body');
-
-function clearForecast() {
-  forecast-body.innerHTML = '';
-}
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const city = document.getElementById('city').value;
-
-  // fetch current weather data
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-      
-      const temp = data.main.temp;
-      const humidity = data.main.humidity;
-      const windSpeed = data.wind.speed;
-      const uvIndex = data.uvi;
-      const visibility = data.visibility;
-      const weatherInfo = `Temperature: ${temp}°C | Humidity: ${humidity}% | Wind Speed: ${windSpeed} m/s | UV Index: ${uvIndex} | Visibility: ${visibility}m`;
-      currentWeather.textContent = weatherInfo;
-    })
-    .catch(error => {
-      currentWeather.textContent = 'Error retrieving current weather data';
-      console.error(error);
-    });
-  
-  // fetch 5-day forecast data
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-      // filter out forecasts for every 24 hours
-      const forecasts = data.list.filter((forecast, index) => index % 8 === 0);
-      forecasts.forEach(forecast => {
-        const date = forecast.dt_txt.split(' ')[0];
+      const forecastList = data.list;
+      for (let i = 0; i < forecastList.length; i += 8) {
+        const forecast = forecastList[i];
+        const date = new Date(forecast.dt_txt);
         const temp = forecast.main.temp;
         const humidity = forecast.main.humidity;
         const windSpeed = forecast.wind.speed;
         const uvIndex = forecast.uvi;
-        const visibility = forecast.visibility;
-        const row = document.createElement('tr');
-        row.innerHTML = `<td>${date}</td><td>${temp}°C</td><td>${humidity}%</td><td>${windSpeed} m/s</td><td>${uvIndex}</td><td>${visibility}m</td>`;
-        forecastTable.appendChild(row);
-      });
+        const forecastElement = document.createElement('div');
+        forecastElement.innerHTML = `
+          <h3>${date.toDateString()}</h3>
+          <p>Temperature: ${temp} &#8451;</p>
+          <p>Humidity: ${humidity}%</p>
+          <p>Wind Speed: ${windSpeed} m/s</p>
+          <p>UV Index: ${uvIndex}</p>
+        `;
+        forecastDiv.appendChild(forecastElement);
+      }
+
+      const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+      if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        displaySearchHistory();
+      }
+
+      
     })
     .catch(error => {
-      forecastTable.textContent = 'Error retrieving 5-day forecast data';
-      console.error(error);
+      console.log(error);
+      forecastDiv.innerHTML = '<p>Sorry, city not found</p>';
     });
-});
+
+    getCurrentWeather(city);
+  
+}
+
+  /* Current Data */
+  function getCurrentWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const date = new Date(data.dt * 1000);
+        const temp = data.main.temp;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+        const uvIndex = data.uvi;
+        const currentWeatherElement = document.createElement('div');
+        currentWeatherElement.innerHTML = `
+          <h3>Current Weather in ${city}</h3>
+          <p>${date.toDateString()}</p>
+          <p>Temperature: ${temp} &#8451;</p>
+          <p>Humidity: ${humidity}%</p>
+          <p>Wind Speed: ${windSpeed} m/s</p>
+          <p>UV Index: ${uvIndex}</p>
+        `;
+        const currentContainer = document.getElementById('current-container');
+        currentContainer.innerHTML = '';
+        currentContainer.appendChild(currentWeatherElement);
+      })
+      .catch(error => console.log(error));
+  }
+
+
+
+function displaySearchHistory() {
+  const searchHistoryDiv = document.getElementById('search-history');
+  const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+  searchHistoryDiv.innerHTML = '';
+
+  searchHistory.forEach(city => {
+    const button = document.createElement('button');
+    button.textContent = city;
+    button.addEventListener('click', () => {
+      document.getElementById('city').value = city;
+      handleFormSubmit(event);
+    });
+    searchHistoryDiv.appendChild(button);
+  });
+}
+const form = document.querySelector('form');
+form.addEventListener('submit', handleFormSubmit);
+
+
+displaySearchHistory();
