dfbe4ac66d6db586523e38c61d4a69a879f01397
Jessica Sanchez <jessamesan@hotmail.com>
2023-04-21 23:26:36 -0600

    feat: adds Search history function and local storage usage

--- assets/script.js
+++ assets/script.js
@@ -1,111 +1,94 @@
-const apiKey = 'b9e5d754e2eb97d65075459b2875fbf5';
-const forecastDiv = document.getElementById('forecast');
-var searchHistoryEl = document.getElementById("search-history")
+// var apiKey = 'c8a50d3cad8319246b6b9747e912dec7';
 
 
-function clearForecast() {
-  forecastDiv.innerHTML = '';
-}
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-  const city = document.getElementById('city').value;
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  clearForecast();
+/*function getWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const forecastList = data.list;
-      for (let i = 0; i < forecastList.length; i += 8) {
-        const forecast = forecastList[i];
-        const date = new Date(forecast.dt_txt);
+      const temperature = data.main.temp;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+      const uvIndex = data.visibility;
+      const clouds = data.clouds.all;
+      const resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = `
+        <h3>${city}</h3>
+        <p>Temperature: ${temperature} °C</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+        <p>UV Index: ${uvIndex}</p>
+        <p>Clouds: ${clouds}%</p>
+      `;
+    })
+    .catch(error => {
+      console.error('Error fetching weather data:', error);
+      const resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = '<p>Error fetching weather data</p>';
+    });
+}
+
+const form = document.querySelector('form');
+form.addEventListener('click', event => {
+  event.preventDefault();
+  const cityInput = document.getElementById('city');
+  const city = cityInput.value;
+  getWeather(city);
+});
+*/
+const API_KEY = 'c8a50d3cad8319246b6b9747e912dec7';
+const form = document.querySelector('form');
+const currentWeather = document.getElementById('current-weather');
+const forecastTable = document.getElementById('forecast-body');
+
+function clearForecast() {
+  forecast-body.innerHTML = '';
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const city = document.getElementById('city').value;
+
+  // fetch current weather data
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      
+      const temp = data.main.temp;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+      const uvIndex = data.uvi;
+      const visibility = data.visibility;
+      const weatherInfo = `Temperature: ${temp}°C | Humidity: ${humidity}% | Wind Speed: ${windSpeed} m/s | UV Index: ${uvIndex} | Visibility: ${visibility}m`;
+      currentWeather.textContent = weatherInfo;
+    })
+    .catch(error => {
+      currentWeather.textContent = 'Error retrieving current weather data';
+      console.error(error);
+    });
+  
+  // fetch 5-day forecast data
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      // filter out forecasts for every 24 hours
+      const forecasts = data.list.filter((forecast, index) => index % 8 === 0);
+      forecasts.forEach(forecast => {
+        const date = forecast.dt_txt.split(' ')[0];
         const temp = forecast.main.temp;
         const humidity = forecast.main.humidity;
         const windSpeed = forecast.wind.speed;
         const uvIndex = forecast.uvi;
-        const forecastElement = document.createElement('div');
-        forecastElement.innerHTML = `
-          <h3>${date.toDateString()}</h3>
-          <p>Temperature: ${temp} &#8451;</p>
-          <p>Humidity: ${humidity}%</p>
-          <p>Wind Speed: ${windSpeed} m/s</p>
-          <p>UV Index: ${uvIndex}</p>
-        `;
-        forecastDiv.appendChild(forecastElement);
-      }
+        const visibility = forecast.visibility;
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${date}</td><td>${temp}°C</td><td>${humidity}%</td><td>${windSpeed} m/s</td><td>${uvIndex}</td><td>${visibility}m</td>`;
+        forecastTable.appendChild(row);
+      });
     })
-    .catch(error => console.log(error));
-
-    getCurrentWeather(city);
-  
-}
-
-  /* Current Data */
-  function getCurrentWeather(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const date = new Date(data.dt * 1000);
-        const temp = data.main.temp;
-        const humidity = data.main.humidity;
-        const windSpeed = data.wind.speed;
-        const uvIndex = data.uvi;
-        const currentWeatherElement = document.createElement('div');
-        currentWeatherElement.innerHTML = `
-          <h3>Current Weather in ${city}</h3>
-          <p>${date.toDateString()}</p>
-          <p>Temperature: ${temp} &#8451;</p>
-          <p>Humidity: ${humidity}%</p>
-          <p>Wind Speed: ${windSpeed} m/s</p>
-          <p>UV Index: ${uvIndex}</p>
-        `;
-        forecastDiv.appendChild(currentWeatherElement); // append currentWeatherElement to the forecastDiv element
-      })
-      .catch(error => console.log(error));
-  }
-
-const form = document.querySelector('form');
-form.addEventListener('submit', handleFormSubmit);
-
-
-
-/* Searched history section NOT WORKING */
-var loadCities = function() {
-  var citiesLoaded = localStorage.getItem("cities")
-  if(!citiesLoaded) {
-      return false;
-  }
-  
-  citiesLoaded = JSON.parse(citiesLoaded);
-  
-  for (var i=0; i < citiesLoaded.length; i++) {
-      displaySearchedCities(citiesLoaded[i])
-      cities.push(citiesLoaded[i])
-  }
-}
-
-var saveCities = function() {
-  localStorage.setItem("cities", JSON.stringify(cities));
-}
-
-var displaySearchedCities = function(city) {
-  var cityCardEl = document.createElement("div");
-  cityCardEl.setAttribute("class", "card");
-  var cityCardNameEl = document.createElement("div");
-  cityCardNameEl.setAttribute("class", "card-body searched-city");
-  cityCardNameEl.textContent = city;
-  
-  cityCardEl.appendChild(cityCardNameEl)
-
-  cityCardEl.addEventListener("click", function () {
-      getCityData(city)
-  });
-
-  searchHistoryEl.appendChild(cityCardEl)
-
-}
-
-//load previously searched cities on page load
-loadCities()
+    .catch(error => {
+      forecastTable.textContent = 'Error retrieving 5-day forecast data';
+      console.error(error);
+    });
+});
